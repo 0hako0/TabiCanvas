10a8b6fa06b7b33607655d95521fd1630c01ade9
0hako0 <haru0226kendo@icloud.com>
2026-07-31 12:37:26 +0900

    Adjust map collage refresh interval

--- src/App.tsx
+++ src/App.tsx
@@ -173,7 +173,7 @@ export default function App() {
     void loadMapCollagePhotos(couple.id, { excludePrevious: true });
     const timer = window.setInterval(() => {
       void loadMapCollagePhotos(couple.id, { animated: true, excludePrevious: true });
-    }, 15_000);
+    }, 5_000);
     return () => window.clearInterval(timer);
   }, [couple?.id, activeMobileView]);
 
